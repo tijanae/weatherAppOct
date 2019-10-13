5e1e01f892ee32d0ffc07049506b3b4618506faf
Tia Lendor <TiaLendon@pursuit.org>
2019-10-13 14:00:09 -0400

    another commit

--- weatherApp/Helpers/ZipCodeHelper.swift
+++ weatherApp/Helpers/ZipCodeHelper.swift
@@ -1,8 +1,8 @@
 //
-//  WeatherModel.swift
+//  ZipCodeHelper.swift
 //  weatherApp
 //
-//  Created by Tia Lendor on 10/9/19.
+//  Created by Tia Lendor on 10/10/19.
 //  Copyright © 2019 Tia Lendor. All rights reserved.
 //
 
